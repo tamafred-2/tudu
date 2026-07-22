86ecae1a1b90c088a36784ac9bbdafaf4f552dd7
Al Fred <tamafred2@gmail.com>
2026-07-23 01:07:27 +0800

    bump: version 1.1.1+3 release build

--- lib/features/settings/application/update_provider.dart
+++ lib/features/settings/application/update_provider.dart
@@ -26,7 +26,7 @@ class UpdateInfo {
 }
 
 class UpdateProvider with ChangeNotifier {
-  static const String currentAppVersion = '1.1.0';
+  static const String currentAppVersion = '1.1.1';
   static const String githubRepo = 'tamafred-2/tudu';
 
   bool _isChecking = false;
